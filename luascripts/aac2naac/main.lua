9f9ad933ba49502469bb330588a2fc5eda6f6b0d
LuMariGames <mariogames0120@gmail.com>
2026-05-21 18:53:21 +0900

    Update main.lua

--- luascripts/aac2naac/main.lua
+++ luascripts/aac2naac/main.lua
@@ -89,7 +89,7 @@ local function convert_to_naac(aac_path, naac_path)
     f_out:write("\0\0\0")
 
     -- 13-14: サンプリングレート 32000 (0x007D) リトルエンディアン
-    f_out:write(string.char(0x7D, 0x00))
+    f_out:write(string.char(0x00, 0x7D))
 
     -- 15-16: 予約領域
     f_out:write("\0\0")
@@ -104,6 +104,7 @@ local function convert_to_naac(aac_path, naac_path)
     write_u32_le(f_out, aac_size)
 
     -- 41-4096: 残りは 0x00 のまま
+    f_out:seek("set", 4096)
 
     -- 6. AAC 本体を追記
     local data = f_in:read("a")
